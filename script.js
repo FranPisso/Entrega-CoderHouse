6240cdefe45bffbe4e18ecb5db954f327d5cccc0
Luca Pisso <147449846+LucaPisso@users.noreply.github.com>
2024-06-08 02:02:12 -0300

    Entrega Final Franco Pisso

--- script.js
+++ script.js
@@ -1,18 +1,12 @@
-let listaProductos = [
-    { id: 2, nombre: "Air jordan 1", marca: "jordan", precio: 220, stock: 2, rutaImagen: "airman.webp", },
-    { id: 6, nombre: "Jordan 6", marca: "jordan", precio: 226, stock: 5, rutaImagen: "jordannnnn.webp", },
-    { id: 67, nombre: "Dunk", marca: "nike", precio: 500, stock: 11, rutaImagen: "airform.webp", },
-    { id: 22, nombre: "Dunk BnW", marca: "nike", precio: 600, stock: 33, rutaImagen: "panda.webp", },
-    { id: 29, nombre: "Travis low", marca: "jordan", precio: 700, stock: 45, rutaImagen: "travisssss.webp", },
-    { id: 33, nombre: "Jordan 4 green", marca: "jordan", precio: 150, stock: 64, rutaImagen: "nabueno.webp", },
-    { id: 12, nombre: "jordan 4 mid", marca: "jordan", precio: 200, stock: 23, rutaImagen: "lambo.webp", },
-    { id: 66, nombre: "Jordan 5 high", marca: "jordan", precio: 700, stock: 12, rutaImagen: "jrdn.webp", },
-    { id: 99, nombre: "Exotic Nike", marca: "nike", precio: 650, stock: 12, rutaImagen: "exotic.webp", }
-]
+const obtenerCarritoLS = () => JSON.parse(localStorage.getItem("carrito")) || []
 
-principal(listaProductos)
+principal()
 
-function principal(productos) {
+async function principal() {
+
+    const response = await fetch("./data.json")
+    const productos = await response.json()
+
     let carrito = obtenerCarritoLS()
     renderizarCarrito()
 
@@ -27,16 +21,13 @@ function principal(productos) {
     botonComprar.addEventListener("click", finalizarCompra)
 }
 
-function obtenerCarritoLS() {
-    let carrito = []
-    let carritoLS = JSON.parse(localStorage.getItem("carrito"))
-    if (carritoLS) {
-        carrito = carritoLS
-    }
-    return carrito
-}
-
 function finalizarCompra() {
+    Swal.fire({
+        title: 'Gracias por comprar!',
+        text: 'para continuar',
+        icon: 'success',
+        confirmButtonText: 'Ok'
+    })
     localStorage.removeItem("carrito")
     renderizarCarrito([])
 }
@@ -136,22 +127,102 @@ function renderizarCarrito() {
         tarjetaProductoCarrito.className = "tarjetaProductoCarrito"
         tarjetaProductoCarrito.id = `tarjetaProductoCarrito${producto.id}`
 
-        tarjetaProductoCarrito.innerHTML = 
-        `
+        tarjetaProductoCarrito.innerHTML = `
         <p>${producto.nombre}</p>
         <p>${producto.precioUnitario}</p>
-        <p>${producto.unidades}</p>
-        <p>${producto.subtotal}</p>
+        <div class=unidades>
+         <button id=restar${producto.id}>-</button>
+         <p>${producto.unidades}</p>
+         <button id=sumar${producto.id}>+</button>
+        </div>
+        <p>${"$" + producto.subtotal}</p>
         <button id=eliminar${producto.id}>Eliminar</button>
         `
         contenedorCarrito.appendChild(tarjetaProductoCarrito)
 
+        let botonRestarUnidad = document.getElementById(`restar${producto.id}`)
+        botonRestarUnidad.addEventListener("click", restarUnidad)
+
+        let botonSumarUnidad = document.getElementById(`sumar${producto.id}`)
+        botonSumarUnidad.addEventListener("click", (e) => sumarUnidad(e, producto.id))
+
         let botonEliminar = document.getElementById(`eliminar${producto.id}`)
         botonEliminar.addEventListener("click", eliminarProductoDeCarrito)
 
-        function eliminarProductoDeCarrito(e) {
-            e.target.parentElement.remove()
-        }
     })
 }
 
+function restarUnidad(e) {
+    let carrito = obtenerCarritoLS()
+    let id = Number(e.target.id.substring(6))
+    let posicionProductoEnCarrito = carrito.findIndex(producto => producto.id === id)
+
+    if (carrito[posicionProductoEnCarrito].unidades > 1) {
+        carrito[posicionProductoEnCarrito].unidades--
+        carrito[posicionProductoEnCarrito].subtotal = carrito[posicionProductoEnCarrito].unidades * carrito[posicionProductoEnCarrito].precioUnitario
+        localStorage.setItem("carrito", JSON.stringify(carrito))
+        renderizarCarrito()
+    } else {
+        eliminarProductoDeCarrito(e)
+    }
+}
+
+function sumarUnidad(e, id) {
+    let carrito = obtenerCarritoLS()
+    let posicionProductoEnCarrito = carrito.findIndex(producto => producto.id === id)
+    let productos = obtenerProductos()
+    let productoOriginal = productos.find(producto => producto.id === id)
+
+    if (carrito[posicionProductoEnCarrito].unidades < productoOriginal.stock) {
+        carrito[posicionProductoEnCarrito].unidades++
+        carrito[posicionProductoEnCarrito].subtotal = carrito[posicionProductoEnCarrito].unidades * carrito[posicionProductoEnCarrito].precioUnitario
+        localStorage.setItem("carrito", JSON.stringify(carrito))
+        renderizarCarrito()
+    } else {
+        Swal.fire({
+            icon: "error",
+            title: "Oops...",
+            text: "No hay suficiente stock!",
+        });
+    }
+}
+
+function eliminarProductoDeCarrito(e) {
+    let carrito = obtenerCarritoLS()
+    let id = Number(e.target.id.substring(8))
+    carrito = carrito.filter(producto => producto.id !== id)
+    localStorage.setItem("carrito", JSON.stringify(carrito))
+    e.target.parentElement.remove()
+}
+
+function eliminarProductoDeCarrito(e) {
+    let carrito = obtenerCarritoLS()
+    let id = Number(e.target.id.substring(8))
+    carrito = carrito.filter(producto => producto.id !== id)
+    localStorage.setItem("carrito", JSON.stringify(carrito))
+    renderizarCarrito()
+}
+
+function obtenerProductos() {
+    return [
+        { id: 2, nombre: "Air jordan 1", marca: "jordan", precio: 220, stock: 2, rutaImagen: "airman.webp", },
+        { id: 6, nombre: "Jordan 6", marca: "jordan", precio: 226, stock: 5, rutaImagen: "jordannnnn.webp", },
+        { id: 67, nombre: "Dunk", marca: "nike", precio: 500, stock: 11, rutaImagen: "airform.webp", },
+        { id: 22, nombre: "Dunk BnW", marca: "nike", precio: 600, stock: 33, rutaImagen: "panda.webp", },
+        { id: 29, nombre: "Travis low", marca: "jordan", precio: 700, stock: 45, rutaImagen: "travisssss.webp", },
+        { id: 33, nombre: "Jordan 4 green", marca: "jordan", precio: 150, stock: 64, rutaImagen: "nabueno.webp", },
+        { id: 12, nombre: "jordan 4 mid", marca: "jordan", precio: 200, stock: 23, rutaImagen: "lambo.webp", },
+        { id: 66, nombre: "Jordan 5 high", marca: "jordan", precio: 700, stock: 12, rutaImagen: "jrdn.webp", },
+        { id: 99, nombre: "Exotic Nike", marca: "nike", precio: 650, stock: 12, rutaImagen: "exotic.webp", }
+    ]
+}
+
+async function pedirInfo() {
+    try {
+        const response = await fetch("./data.json")
+        const productos = await response.json()
+        return productos
+    } catch (error) {
+        console.log("algo falló")
+    }
+}
